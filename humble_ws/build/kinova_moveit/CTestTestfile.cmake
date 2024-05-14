1a02b52249f472888f1d61f06c0e6c0ce4dc686e
Keenan88 <lakingsfan08@gmail.com>
2024-05-14 12:28:42 -0400

    Switched base image for arm to official image from moveit, demo runs without errors now. Need to get Kinova Running like demo next

---
+++ humble_ws/build/kinova_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/humble_ws/src/kinova_moveit
+# Build directory: /home/humble_ws/build/kinova_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- humble_ws/src/kinova_arm/setup.py
+++ humble_ws/src/kinova_arm/setup.py
@@ -1,6 +1,10 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'kinova_arm'
+
+
 
 setup(
     name=package_name,
@@ -10,6 +14,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'description/urdf'), glob(os.path.join('description/urdf', 'GEN3_6DOF_VISION_URDF_ARM_V01.urdf'))),
+        (os.path.join('share', package_name, 'description/meshes'), glob(os.path.join('description/meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
