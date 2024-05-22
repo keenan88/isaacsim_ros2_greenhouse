b3238c39948a3930c2812bb0ac1758a5d5c3efb9
keenan88 <lakingsfan08@gmail.com>
2024-05-22 13:33:42 -0400

    Cleaned up unused packages

--- humble_ws/src/kinova_moveit/launch/static_virtual_joint_tfs.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("GEN3_6DOF_VISION_URDF_ARM_V01", package_name="kinova_moveit").to_moveit_configs()
-    return generate_static_virtual_joint_tfs_launch(moveit_config)
