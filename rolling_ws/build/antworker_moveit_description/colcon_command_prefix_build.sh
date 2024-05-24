1eb6f7a48ef9428dea72d1aba05cb461bf669042
keenan88 <lakingsfan08@gmail.com>
2024-05-24 16:06:13 -0400

    Arm in RVIZ working, with seperated workspaces. Still need to integrate into simulation

---
+++ rolling_ws/build/antworker_moveit_description/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/rolling_ws/install/antworker_description/share/antworker_description/package.sh"
---
+++ rolling_ws/build/attempt_py_moveit/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rolling_ws/install/attempt_py_moveit'
