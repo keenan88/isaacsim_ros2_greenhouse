af4727c9a703cf60a489e5ba620e0f484ea6bd25
keenan88 <lakingsfan08@gmail.com>
2024-05-28 10:01:31 -0400

    Added moveit2 2.9.0 from source and built it. All modules built successfully

--- docker_entry_scripts/joint_state_translater_entrypoint.sh
+++ docker_entry_scripts/joint_state_translater_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-# gnome-terminal
+gnome-terminal
 
-cd /home/humble_ws/
-colcon build --packages-select moveit_rolling_humble_interface
-source install/setup.bash
-ros2 run moveit_rolling_humble_interface joint_state_translater
+#cd /home/humble_ws/
+#colcon build --packages-select moveit_rolling_humble_interface
+#source install/setup.bash
+#ros2 run domain_bridge domain_bridge /home/humble_ws/src/moveit_rolling_humble_interface/config/bridge_config.yaml
 
 
 
