bad011159d6e2943546e76c25dcca7d53a7c4b97
keenan88 <lakingsfan08@gmail.com>
2024-06-10 12:16:38 -0400

    Added chassis -> wheels transform publish to ROS2 in isaacsim

--- humble_ws/src/obstacle_avoidance_k_demo.launch.py
+++ humble_ws/src/obstacle_avoidance_k_demo.launch.py
@@ -195,9 +195,11 @@ def generate_launch_description():
             rviz_node,
             robot_state_publisher,
             move_group_node,
+
             ros2_control_node,
             joint_state_broadcaster_spawner,
             panda_arm_controller_spawner,
+            
             container
         ]
     )
