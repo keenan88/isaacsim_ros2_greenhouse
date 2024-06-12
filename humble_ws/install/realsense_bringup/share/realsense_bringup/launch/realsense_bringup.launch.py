62df64843bd784723e0445f3c4fc4b24c4c52658
keenan88 <lakingsfan08@gmail.com>
2024-06-12 15:03:02 -0400

    Added seperate launch for realsense with its own domain ID, so I can forward only the otopics and transforms we want

---
+++ humble_ws/install/realsense_bringup/share/realsense_bringup/launch/realsense_bringup.launch.py
@@ -0,0 +1,45 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    is_simulation = os.getenv('USE_SIM')
+    if is_simulation == 'False': is_simulation = False
+    else: is_simulation = True
+
+    realsense_pointcloud_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('realsense2_camera'), 'examples', 'pointcloud'),
+            '/rs_d455_pointcloud_launch.py'
+        ])
+    )
+
+
+    arm_base_to_camera_static_tf = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='arm_base_to_camera_static_tf',
+            output='screen',
+            arguments=[
+                '1.0', '2.0', '3.0',       # translation (x, y, z)
+                '0.0', '0.0', '0.0', '1.0', # rotation (quaternion: x, y, z, w)
+                'arm_base_link',                # parent frame
+                'laser'                     # child frame
+            ]
+        )
+
+    
+
+    return LaunchDescription(
+        [
+            realsense_pointcloud_launch
+        ]    
+    )
