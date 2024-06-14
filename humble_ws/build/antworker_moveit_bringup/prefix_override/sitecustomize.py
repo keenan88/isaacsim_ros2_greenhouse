6602f636c122b9aebae3f6eddf2efbecd4068a05
keenan88 <lakingsfan08@gmail.com>
2024-06-14 11:17:49 -0400

    Added gitignore

--- humble_ws/build/antworker_moveit_bringup/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/humble_ws/install/antworker_moveit_bringup'
--- humble_ws/install/antworker_moveit_description/share/antworker_moveit_description/launch/spawn_controllers.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("antworker", package_name="antworker_moveit_description").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
