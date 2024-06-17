7263e725e6943d879154a4ef81b22628ba813828
keenan88 <lakingsfan08@gmail.com>
2024-06-17 15:15:46 -0400

    OMPL planner succeeds in going from ahead of wall to behind wall. Likely requires more tuning, path is not particularly direct

--- humble_ws/src/antworker_moveit_bringup/launch/moveit_bringup.launch.py
+++ humble_ws/src/antworker_moveit_bringup/launch/moveit_bringup.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
             file_path="config/moveit_controllers.yaml"
         )
         .planning_pipelines(
-            pipelines=["pilz_industrial_motion_planner", "chomp"]
+            pipelines=["chomp", "pilz_industrial_motion_planner", "ompl"]
         )
         .moveit_cpp(
             file_path = "/home/humble_ws/src/antworker_moveit_bringup/config/moveit_py_params.yaml"
