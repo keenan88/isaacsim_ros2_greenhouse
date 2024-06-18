c492e9ba3f5549e9a0f2be2402ebf358a0ab8879
keenan88 <lakingsfan08@gmail.com>
2024-06-18 16:37:41 -0400

    Physical arm moving with EE, but EE itself not moving. Lets see if we can fully get rid of EE in code

--- humble_ws/src/antworker_moveit_bringup/launch/moveit_bringup.launch.py
+++ humble_ws/src/antworker_moveit_bringup/launch/moveit_bringup.launch.py
@@ -27,42 +27,30 @@ def generate_launch_description():
     )
 
     launch_arguments = {
-        "robot_ip": "xxx.yyy.zzz.www",
-        "use_fake_hardware": "true",
+        "robot_ip": "192.168.1.10",
+        "use_fake_hardware": "false",
         "gripper": "robotiq_2f_85",
         "dof": "6",
     }
 
     moveit_config = (
         MoveItConfigsBuilder(
-            robot_name="gen3", 
-            package_name="kinova_gen3_6dof_robotiq_2f_85_moveit_config",
+            "gen3", package_name="kinova_gen3_6dof_robotiq_2f_85_moveit_config"
         )
         .robot_description(mappings = launch_arguments)
-        # .robot_description(
-        #     file_path="config/GEN3_6DOF_VISION_URDF_ARM_V01.urdf.xacro"
-        # )
-        # .robot_description_semantic(
-        #     file_path="config/gen3.srdf"
-        # )
-        # .planning_scene_monitor(
-        #     publish_robot_description = True, publish_robot_description_semantic = True
-        # )
-        # .trajectory_execution(
-        #     file_path="config/moveit_controllers.yaml"
-        # )
-        # .planning_pipelines(
-        #     pipelines=["chomp", "pilz_industrial_motion_planner", "ompl"]
-        # )
-        # .moveit_cpp(
-        #     file_path = "/home/humble_ws/src/antworker_moveit_bringup/config/moveit_py_params.yaml"
-        # )
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(
+            pipelines=["ompl", "pilz_industrial_motion_planner"]
+        )
         .to_moveit_configs()
     )
 
 
     # Start the actual move_group node/action server
-    move_group_node = Node(
+    run_move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
@@ -95,68 +83,66 @@ def generate_launch_description():
         ],
     )
 
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["--frame-id", "world", "--child-frame-id", "base_link"],
+    )
+
     # Publish TF
-    # robot_state_publisher = Node(
-    #     package="robot_state_publisher",
-    #     executable="robot_state_publisher",
-    #     name="robot_state_publisher",
-    #     output="both",
-    #     parameters=[
-    #         moveit_config.robot_description,
-    #         {"use_sim_time": is_simulation}    
-    #     ],
-    # )
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_config.robot_description],
+    )
 
-    # ros2_controllers_path = os.path.join(
-    #     get_package_share_directory("antworker_moveit_description2"),
-    #     "config",
-    #     "ros2_controllers.yaml",
-    # )
-    
-    # ros2_control_node = Node(
-    #     package="controller_manager",
-    #     executable="ros2_control_node",
-    #     parameters=[
-    #         ros2_controllers_path,
-    #         {"use_sim_time": is_simulation}
-    #     ],
-    #     remappings=[
-    #         ("/controller_manager/robot_description", "/robot_description"),
-    #     ],
-    #     output="screen",
-    # )
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("kinova_gen3_6dof_robotiq_2f_85_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
 
-    # joint_state_broadcaster_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=[
-    #         "joint_state_broadcaster",
-    #         "--controller-manager",
-    #         "/controller_manager",
-    #     ],
-    # )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[ros2_controllers_path],
+        remappings=[
+            ("/controller_manager/robot_description", "/robot_description"),
+        ],
+        output="both",
+    )
 
-    # kinova_arm_controller_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
-    # )
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
 
-    # ctrl_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([
-    #         os.path.join(
-    #             get_package_share_directory('antworker_moveit_description2'),
-    #             'launch',
-    #             'spawn_controllers.launch.py'
-    #         )
-    #     ])
-    # )
+    arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+    )
+
+    hand_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["robotiq_gripper_controller", "-c", "/controller_manager"],
+    )
 
     sim_launch = [
         rviz_config_arg,
         rviz_node,
         # robot_state_publisher,
-        move_group_node,
+        run_move_group_node,
         # ros2_control_node,
         # joint_state_broadcaster_spawner,
         # kinova_arm_controller_spawner
@@ -165,11 +151,13 @@ def generate_launch_description():
     hw_launch = [
         rviz_config_arg,
         rviz_node,
-        # robot_state_publisher,
-        move_group_node,
-        # ros2_control_node,    
-        # joint_state_broadcaster_spawner,
-        # kinova_arm_controller_spawner,
+        static_tf,
+        robot_state_publisher,
+        run_move_group_node,
+        ros2_control_node,
+        joint_state_broadcaster_spawner,
+        arm_controller_spawner,
+        hand_controller_spawner,
     ]
 
     launch = sim_launch if is_simulation else hw_launch
