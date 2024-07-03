f6e12289e9e1e9b9d628f8dffc1b9e1450597f7e
Keenan88 <lakingsfan08@gmail.com>
2024-07-03 09:33:13 -0400

    Pipe rails added to simulation. Robots green wheel sinking into pipe rails a bit, but will ignore for now

--- docker_entry_scripts/isaacsim_entrypoint.sh
+++ docker_entry_scripts/isaacsim_entrypoint.sh
@@ -2,8 +2,8 @@
 
 source /opt/ros/humble/setup.bash
 
-# if [ "$USE_SIM" == "True" ]; then
-#     ./runheadless.native.sh --reset-data
-# fi
+if [ "$USE_SIM" == "True" ]; then
+    ./runheadless.native.sh --reset-data
+fi
 
 exec bash
