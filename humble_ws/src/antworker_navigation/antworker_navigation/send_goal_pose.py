248f79de9b2ba0c429d95166d7f1f8aa7e03a0f9
Keenan88 <lakingsfan08@gmail.com>
2024-07-03 14:53:22 -0400

    Removed wheel velocity maximums from Diff contrller in isaacsim action graph

--- humble_ws/src/antworker_navigation/antworker_navigation/send_goal_pose.py
+++ humble_ws/src/antworker_navigation/antworker_navigation/send_goal_pose.py
@@ -19,7 +19,7 @@ class GoalPoseServer(Node):
 
         self.pose_stamped.header.frame_id = 'map'
         self.pose_stamped.header.stamp = self.get_clock().now().to_msg()
-        self.pose_stamped.pose.position.y = -3.0 # Assumes robot starts at (0,0,0), (0,0,0)
+        self.pose_stamped.pose.position.y = -20.0 # Assumes robot starts at (0,0,0), (0,0,0)
 
         self.pose_stamped.pose.orientation.z = -0.73
         self.pose_stamped.pose.orientation.w = 0.67
